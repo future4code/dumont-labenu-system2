8a146d7c237ba67f4380e141bb261c9b5e9f98f4
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2021-01-19 17:11:39 -0300

    base do projeto

---
+++ src/routes/router.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = express.Router();
+
+//Controller
+// import { getAllUsers, getUsersByName, getUsersByType, getUsersOrderedByNameOrType, getAllUsersPaginate } from '../controllers/UsersController';
+
+//Rotas usuários
+// router.get("/user", getAllUsers);
+// router.get("/user/:name", getUsersByName);
+// router.get("/users/:type", getUsersByType);
+// router.get("/users-order", getUsersOrderedByNameOrType);
+// router.get("/users-paginate", getAllUsersPaginate);
+
+export default router;
